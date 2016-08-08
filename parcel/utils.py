f5f8012ebb11147f7959e48cfc7ecf525fe5367a
Christian Dompierre <cdompierre@uchicago.edu>
2016-08-08 18:05:00 +0000

    chore(download): Add '.partial' extention
    Adds a temporary extention to indicate file download still in progres
    
    Addresses DTT-1

--- parcel/utils.py
+++ parcel/utils.py
@@ -92,6 +92,22 @@ def set_file_length(path, length):
         raise Exception('Unable to set file length: {}'.format(str(e)))
 
 
+def remove_partial_extension(path):
+    try:
+        if path[-8:] != '.partial':
+            log.warn('No partial extension found')
+            log.warn('Got {}'.format(path))
+            return
+        log.info('renaming to {}'.format(path[:-8]))
+        os.rename(path, path[:-8])
+    except Exception as e:
+        raise Exception('Unable to remove partial extension: {}'.format(str(e)))
+
+
+def check_file_existence_and_size(path, size):
+    return (os.path.isfile(path) and os.path.getsize(path) == size)
+
+
 def get_file_type(path):
     try:
         mode = os.stat(path).st_mode
